12079cc9ce8f20c7609264cab49cf79985b58b42
ayeshaamin470 <77930530+ayeshaamin470@users.noreply.github.com>
2021-02-07 13:56:56 +0400

    Add files via upload

---
+++ task3.php
@@ -0,0 +1,12 @@
+<?php  
+ $a = 14;  
+ $b= 12;  
+ $Area = $a*$b;
+$Perimeter = 2*($a+$b);
+
+ echo "area of rectangle is $length * $width= " .$Area . "<br />";  
+  echo "area of Perimeter  is $length * $width= " .$Perimeter . "<br />";  
+  if ($a=$b) {
+  echo "This is square";
+  }
+  ?>  
