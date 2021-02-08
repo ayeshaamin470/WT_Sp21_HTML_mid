2fcd306903300edf68fbd5f76c49006bfae31513
ayeshaamin470 <77930530+ayeshaamin470@users.noreply.github.com>
2021-02-08 09:32:43 +0400

    Add files via upload

---
+++ lab3task1/task4.php
@@ -0,0 +1,11 @@
+<?php
+    $name = "raju";
+    if($name == "raju" || $name == "mina" || $name == "mithu")
+     {
+        echo "<b>hello</b>";
+     }
+
+
+    else echo "you are not recognized";
+
+     ?>
